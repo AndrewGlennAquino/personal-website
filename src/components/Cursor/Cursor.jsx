5562be62b4bd7ed474edb7bf5133d0ad0ab85b80
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-03 17:56:29 -0500

    Add custom cursor

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -1,6 +1,30 @@
+import { useState, useEffect } from "react";
 import "./Cursor.css";
 
 function Cursor() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  const cursorPosition = {
+    left: position.x,
+    top: position.y,
+  };
+
+  return <div className="cursor" style={cursorPosition}></div>;
 }
 
 export default Cursor;
