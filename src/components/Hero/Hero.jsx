1dcb73ce74953d191099d814795f1dcd3ad92d8b
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-06 13:04:39 -0500

    Remove cursor and improve typewritter effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,68 +2,48 @@ import { motion } from "motion/react";
 import "./Hero.css";
 import Moon from "../Moon/Moon.jsx";
 
+// Animated typewritter component
+function TypeWritter({ string, variants }) {
+  const charArray = string.split("");
+  return charArray.map((char, index) => (
+    <motion.span key={`${char}-${index}`} variants={variants}>
+      {char}
+    </motion.span>
+  ));
+}
+
 // Hero component that holds name, title, and animated moon
 function Hero() {
-  // Animation variants for name
-  const nameVariants = {
-    typing: {
-      width: "fit-content",
-      transition: {
-        duration: 1.5,
-        delay: 1.5,
-        ease: "linear",
-      },
-    },
-    cursor: {
-      borderColor: "rgba(240, 240, 240, 0)",
-      transition: {
-        duration: 0.5,
-        ease: "linear",
-        repeat: 6,
-        repeatType: "reverse",
-      },
-    },
+  // Animation variants for header elements
+  const headerVariants = {
+    init: {},
+    animate: { opacity: 1, transition: { staggerChildren: 0.125 } },
+    animateDelay: { opacity: 1, transition: { staggerChildren: 0.125, delayChildren: 4.5 } }
   };
 
-  // Animation variants for title
-  const titleVariants = {
-    typing: {
-      width: "fit-content",
-      transition: {
-        duration: 1.5,
-        delay: 5.5,
-        ease: "linear",
-      },
-    },
-    cursor: {
-      borderColor: "rgba(240, 240, 240, 1)",
-      transition: {
-        duration: 0.5,
-        delay: 3.5,
-        ease: "linear",
-        repeat: Infinity,
-        repeatType: "reverse",
-      },
-    },
-  }
+  // Animation variants for span elements
+  const spanVariants = {
+    init: { opacity: 0 },
+    animate: { opacity: 1, transition: { opacity: { duration: 0 } } },
+    animateDelay: { opacity: 1, transition: { opacity: { duration: 0 } } },
+  };
 
   return (
     <section className="hero-container">
       <div className="title-container">
-        <motion.h1
-          initial={{ width: "0%" }}
-          animate={["typing", "cursor"]}
-          variants={nameVariants}
-        >
-          Andrew Glenn Aquino
+        <motion.h1 initial="init" animate="animate" variants={headerVariants}>
+          <TypeWritter string={"Andrew Glenn Aquino"} variants={spanVariants} />
         </motion.h1>
-        
+
         <motion.h2
-          initial={{ width: "0%", borderColor: "rgba(240, 240, 240, 0)" }}
-          animate={["typing", "cursor"]}
-          variants={titleVariants}
+          initial="init"
+          animate="animateDelay"
+          variants={headerVariants}
         >
-          Software Developer
+          <TypeWritter
+            string={"Software Developer"}
+            variants={spanVariants}
+          />
         </motion.h2>
       </div>
 
